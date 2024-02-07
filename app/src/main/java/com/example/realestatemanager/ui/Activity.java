a71ec0c7d85b9dc3bd86dacc2e9bbc897294498d
Julien <julien.aboutard@gmail.com>
2024-02-07 17:01:59 +0100

    First Commit

---
+++ app/src/main/java/com/example/realestatemanager/ui/Activity.java
@@ -0,0 +1,23 @@
+package com.example.realestatemanager.ui;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.realestatemanager.R;
+import com.example.realestatemanager.utils.Utils;
+
+public class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+        TextView textView = findViewById(R.id.test);
+        textView.setText(""+Utils.newIsInternetAvailable(this));
+
+    }
+}
---
+++ app/src/main/java/com/example/realestatemanager/utils/Utils.java
@@ -0,0 +1,73 @@
+package com.example.realestatemanager.utils;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Philippe on 21/02/2018.
+ */
+
+public class Utils {
+
+    /**
+     * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * initial valeur de conversion 0.812
+     * On change cette valeur pour correspondre à taux actuelle (07/02/2023) 0.9282
+     * @param dollars
+     * @return
+     */
+    public static int convertDollarToEuro(int dollars){
+        return (int) Math.round(dollars * 0.9282);
+    }
+
+    public static int convertEuroToDollar(int euros) {
+        return (int) Math.round(euros * 1.07729);
+    }
+
+    /**
+     * Conversion de la date d'aujourd'hui en un format plus approprié
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @return
+     */
+    public static String getTodayDate(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        return dateFormat.format(new Date());
+    }
+
+    public static String newGetTodayDate() {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        return dateFormat.format(LocalDate.now());
+    }
+
+    /**
+     * Vérification de la connexion réseau
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param context
+     * @return
+     */
+    public static Boolean isInternetAvailable(Context context){
+        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        return wifi.isWifiEnabled();
+    }
+
+    public static Boolean newIsInternetAvailable(Context context){
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnectedOrConnecting()  ||
+            connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnectedOrConnecting();
+    }
+}
